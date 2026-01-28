cc5e9cc20a4e308d3113c8b431069cab69612a41
EcoHaven Developer <developer@ecohaven.com>
2026-01-29 01:24:22 +0300

    fix: relocate migration command to start.sh for docker

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-# Run migrations (optional, can also be done via preDeployCommand)
-# npx prisma migrate deploy
+# Run migrations
+npx prisma migrate deploy
 
 # Start the application
 node server.js
